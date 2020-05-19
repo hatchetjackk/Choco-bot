9e214e672571ada6dfc782e806e6c03f39e33ff8
Hatchet <rob.marshall06@gmail.com>
2020-05-19 08:05:37 -0400

    better message handling

--- mae-bot.py
+++ mae-bot.py
@@ -2,13 +2,19 @@
 import asyncio
 import json
 import os
+import logging
 from datetime import datetime
 from itertools import cycle
-
 import util.tools as tools
 import util.db as database
 import discord
 from discord.ext import commands
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='files/logs/discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
 
 
 async def prefix(bot, message):
@@ -21,6 +27,7 @@ client.remove_command('help')
 
 @client.command()
 async def reload(ctx, extension_name: str):
+    await ctx.channel.purge(limit=1)
     if not await client.is_owner(ctx.author):
         return
     try:
@@ -28,7 +35,7 @@ async def reload(ctx, extension_name: str):
         client.load_extension('cogs.' + extension_name)
     except (AttributeError, ImportError) as e:
         print(e)
-    await ctx.send(embed=tools.single_embed(f'{extension_name.capitalize()} reloaded'))
+    await ctx.send(embed=tools.single_embed(f'{extension_name.capitalize()} reloaded'), delete_after=3)
     print(f'* Reloaded {extension_name}')
 
 
@@ -39,14 +46,14 @@ async def load(ctx, extension_name: str):
     try:
         client.load_extension('cogs.' + extension_name)
     except (AttributeError, ImportError) as e:
-        print(e)
+        print(f'* Error loading cog {extension_name}: {e}')
     await ctx.send(embed=tools.single_embed(f'{extension_name.capitalize()} loaded'))
     print(f'* Loaded {extension_name}')
 
 
 @client.event
 async def on_ready():
-    print(f'Login successful at {datetime.now()}')
+    print(f'{datetime.now().strftime("%I:%M:%S %p")}\tLogin successful')
     database.check_database()
     status_options = ['with turnips', 'with stalks']
     msg = cycle(status_options)
@@ -58,24 +65,38 @@ async def on_ready():
 
 @client.event
 async def on_resume():
-    print(f'Connection resumed at {datetime.now()}')
+    print(f'{datetime.now().strftime("%I:%M:%S %p")}\tConnection resumed')
+
+
+@client.event
+async def on_command(ctx):
+    msg = f'{datetime.now().strftime("%I:%M:%S %p")}\tUser:[{ctx.author}] Channel:[{ctx.channel.name}] | Triggered Command [{ctx.command}]'
+    print(msg)
 
 
 @client.event
 async def on_command_error(ctx, error):
     if isinstance(error, commands.CommandOnCooldown):
-        await ctx.send(embed=tools.single_embed_tooltip(f'You\'re doing that too fast!\n{error}'))
-    elif isinstance(error, commands.CheckFailure):
-        await ctx.send(embed=tools.single_embed_tooltip(f'I\'m sorry. You do not have permission to run this command.'))
+        msg = f'{datetime.now().strftime("%I:%M:%S %p")}\tUser:[{ctx.author}]\tChannel:[{ctx.channel.name}]\tTriggered cooldown [{ctx.command}]'
+        print(msg)
     elif isinstance(error, commands.CommandNotFound):
-        await ctx.send(embed=tools.single_embed_tooltip(f'I\'m sorry. That command doesn\'t exist. Please'
-                                                        f'check {prefix}help for more commands.'))
+        await ctx.send(embed=tools.single_embed_tooltip(f'I\'m sorry. That command doesn\'t exist.'))
+        msg = f'{datetime.now().strftime("%I:%M:%S %p")}\tUser:[{ctx.author}]\tChannel:[{ctx.channel.name}]\tTriggered CommandNotFound [{ctx.command}]'
+        print(msg)
     elif isinstance(error, commands.CheckFailure):
-        await ctx.send(f'You do not have permission to run this command.', delete_after=10)
+        await ctx.send(embed=tools.single_embed(f'You do not have permission to run this command.'), delete_after=10)
+        msg = f'{datetime.now().strftime("%I:%M:%S %p")}\tUser:[{ctx.author}]\tChannel:[{ctx.channel.name}]\tTriggered permissions error [{ctx.command}]'
+        print(msg)
     elif isinstance(error, discord.errors.NotFound):
-        pass
+        msg = f'{datetime.now().strftime("%I:%M:%S %p")}\tUser:[{ctx.author}]\tChannel:[{ctx.channel.name}]\tTriggered permissions error [{ctx.command}]'
+        print(msg)
+    elif isinstance(error, commands.CommandInvokeError):
+        await ctx.send(embed=tools.single_embed(f'An error occurred when running {ctx.command}.'))
+        msg = f'{datetime.now().strftime("%I:%M:%S %p")}\tUser:[{ctx.author}]\tChannel:[{ctx.channel.name}]\tTriggered CommandInvokeError [{ctx.command}] {error}'
+        print(msg)
     else:
-        print(f'{ctx.author.display_name} encountered a {type(error)} error running {ctx.command} in {ctx.channel.name}: {error}')
+        msg = f'{datetime.now().strftime("%I:%M:%S %p")}\tUser:[{ctx.author}]\tChannel:[{ctx.channel.name}]\tTriggered an error [{ctx.command}:{error}]'
+        print(msg)
 
 
 if __name__ == '__main__':
